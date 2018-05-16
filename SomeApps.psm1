e88181f1f60cffa2761353f308b8778866497d25
Aurimas Navardauskas <aurimas.navardauskas@outlook.com>
2018-05-17 00:08:54 +0300

    commit for jenkins test

--- SomeApps.psm1
+++ SomeApps.psm1
@@ -17,7 +17,7 @@ function Test-ModulePresent {
         return $Result
     }
 }
-
+# just commit
 # Export only the functions using PowerShell standard verb-noun naming.
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
 # This improves performance of command discovery in PowerShell.
